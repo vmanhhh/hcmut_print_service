10600f5467eeb9cc5be0c47c8a79699410575b0a
vmanhhhh <tv.manh0706@gmail.com>
2024-11-22 12:47:35 +0700

    Thêm Button và Dialog

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CustomDialog from './components/CustomDialog';
+import CustomButton from './components/Button';
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <CustomButton text="Open form dialog" onClick={handleOpen} />
+      <CustomDialog
+        open={open}
+        onClose={handleClose}
+        title="Subscribe"
+      >
+        <p>Hello my friends</p>
+      </CustomDialog>
     </div>
   );
 }
