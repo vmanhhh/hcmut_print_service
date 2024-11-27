a70fd5b2d419b7171da7cd5a7dbdbd47348bee92
Kanao <truongphuoc147@gmail.com>
2024-11-27 21:28:27 +0700

    update

--- server/routes/printer.js
+++ server/routes/printer.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const printerController = require('../controller/PrinterController');
 const authenticate = require('../middlewares/authentication');
 
-router.get('/getalls', authenticate, printerController.getAllPrinters);
+router.get('/getalls', printerController.getAllPrinters);
 router.post('/create', authenticate, printerController.createPrinter);
 router.post('/getbyid', authenticate, printerController.getPrinterById);
 router.post('/getbybuilding', authenticate, printerController.getPrintersByBuilding);
