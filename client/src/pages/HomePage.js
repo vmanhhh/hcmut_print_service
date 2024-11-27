3b63d7bf0181138c3c729052107c1f35df067c77
vmanhhhh <tv.manh0706@gmail.com>
2024-11-27 19:48:39 +0700

    Thêm nút bắt đầu in vào Home

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <Link to="/print">
+        <button className="rest-option btn btn-primary" onClick={() => {}}>
+          <span className="text">Bắt đầu in</span>{" "}
+          <i className="fa fa-angle-right"></i>
+        </button>
+      </Link>
     </>
   );
 };
