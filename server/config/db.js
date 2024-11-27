4b45a5a9b37ad56e56d698defa78556c7d65fe08
Kanao <truongphuoc147@gmail.com>
2024-11-27 21:39:05 +0700

    update

--- server/config/db.js
+++ server/config/db.js
@@ -6,13 +6,7 @@ const {
 
 require('dotenv').config()
 // Firebase configuration
-console.log(process.env.API_KEY)
-console.log(process.env.AUTH_DOMAIN)
-console.log(process.env.PROJECT_ID)
-console.log(process.env.STORAGE_BUCKET)
-console.log(process.env.MESSAGING_SENDER_ID)
-console.log(process.env.APP_ID)
-console.log(process.env.MEASUREMENT_ID)
+
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
