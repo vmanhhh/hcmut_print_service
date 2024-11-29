f0da98df4b8fd595a0fafd242c8667e6fea41e75
dang-tuan-3724 <tuan.dangminh372004@hcmut.edu.vn>
2024-11-29 12:46:45 +0700

    [UPDATE] transaction history

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,39 @@
+/////DAY LA FILE DE TUI TEST THU XEM NO CHAY HAY KHONG---LUC GOP CODE LAI ONG XOA DI NHA
 
-import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
 
-import HomePage from "./pages/HomePage";
-import Login from "./components/login/login";
-import InfoDialog from "./components/InfoDialog";
-import BuyPaperPage from "./pages/BuyPaperPage";
-import ProtectedRoute from "./components/ProtectedRoute";
-import "./App.css";
-import Log from "./pages/log/log";
-const App = () => {
-  return (
-      <Router>
-        <div className="app">
-          <Navbar />
-          <Routes>
-            <Route
-              path="/log"
-              element={<Log />}
-            />
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
-  );
-}
+// import React, { useState, useEffect } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   Navigate,
+// } from "react-router-dom";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
+// import Navbar from "./components/Navbar/Navbar";
+// import Footer from "./components/Footer/Footer";
 
-export default App;
+// import HomePage from "./pages/HomePage";
+// import Login from "./components/login/login";
+// import InfoDialog from "./components/InfoDialog";
+// import BuyPaperPage from "./pages/BuyPaperPage";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import "./App.css";
+// import Log from "./pages/log/log";
+// const App = () => {
+//   return (
+//       <Router>
+//         <div className="app">
+//           <Navbar />
+//           <Routes>
+//             <Route
+//               path="/log"
+//               element={<Log />}
+//             />
+//           </Routes>
+//         </div>
+//         <Footer />
+//       </Router>
+//   );
+// }
+
+// export default App;
--- client/src/pages/log/log.js
+++ client/src/pages/log/log.js
@@ -19,11 +19,11 @@ const log = () => {
             </div>
         </div>
 
-        <LogItem date='12/08/2024' name='File: "Software engineering final term...pdf' state='Đã hoàn tất'/>
-        <LogItem date='18/09/2022' name='File: "XSTK midterm...pdf' state='Đã hoàn tất'/> 
-        <LogItem date='21/10/2022' name='File: "Đề thi GHK HK223.pdf' state='Đã hoàn tất'/> 
-        <LogItem date='06/11/2022' name='File: "XSTK midterm...pdf' state='Đã hoàn tất'/> 
-        <LogItem date='22/10/2022' name='File: "Asignment report HK241.pdf' state='Đã hoàn tất'/> 
+        <LogItem date='12/08/2024' name='File: "Software engineering final term...pdf"' state='Đã hoàn tất'/>
+        <LogItem date='18/09/2022' name='File: "XSTK midterm...pdf"' state='Đã hoàn tất'/> 
+        <LogItem date='21/10/2022' name='File: "Đề thi GHK HK223.pdf"' state='Đã hoàn tất'/> 
+        <LogItem date='06/11/2022' name='File: "XSTK midterm...pdf"' state='Đã hoàn tất'/> 
+        <LogItem date='22/10/2022' name='File: "Asignment report HK241.pdf"' state='Đã hoàn tất'/> 
     </div>
   )
 
